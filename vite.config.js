196af184d51c586fc2d9a12f25881787002bb152
halcyonXT <ctsev1987@gmail.com>
2024-07-24 19:06:28 +0200

    config and fonts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/new-portfolio/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      $fonts: resolve('./src/assets')
+    }
+  }
 })
